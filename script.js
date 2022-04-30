a970d52d0adfb46f579812f418f7dfca1531cfd7
Kawwy <kawinmathivanan.u@gmail.com>
2022-05-01 00:29:15 +0530

    Added successfully

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+setInterval(() => {
+
+    let ss = document.getElementById("ss");
+
+    let sec_dots = document.querySelector('.sec_dots');
+
+    let s = new Date().getSeconds();
+
+    ss.style.strokeDashoffset = 1575 - (1575 * s) / 60;
+
+    sec_dots.style.transform = `rotate(${s * 6}deg)`;
+    // 360/60 = 6
+})
 const deg = 6;
-console.log("check");
 const hr = document.querySelector("#hr");
 const mn = document.querySelector("#mn");
-const sc = document.querySelector("#sc");
+
 setInterval(() => {
-	let day = new Date();
-	let hh = day.getHours() * 30;
-	let mm = day.getMinutes() * deg;
-	let ss = day.getSeconds() * deg;
-	hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
-	mn.style.transform = `rotateZ(${mm}deg)`;
-	sc.style.transform = `rotateZ(${ss}deg)`;
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
 })
